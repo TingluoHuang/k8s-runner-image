97064d7e01a8c8acabcd05b141c27fc3f406efd7
Tingluo Huang <tingluohuang@github.com>
2020-09-08 22:42:03 -0400

    Update download-runner.sh

--- download-runner.sh
+++ download-runner.sh
@@ -12,7 +12,29 @@ set -e
 # download actions and unzip it
 #curl -Ls ${RUNNER_DOWNLOAD_URL} | tar xz \
 
-curl -Ls https://github.com/TingluoHuang/runner/releases/download/test/actions-runner-linux-x64-2.299.0.tar.gz | tar xz
+#curl -Ls https://github.com/TingluoHuang/runner/releases/download/test/actions-runner-linux-x64-2.299.0.tar.gz | tar xz
 
 # delete the download tar.gz file
-rm -f ${RUNNER_DOWNLOAD_URL##*/}
+#rm -f ${RUNNER_DOWNLOAD_URL##*/}
+
+# For the GHES Alpha, download the runner from github.com
+latest_version_label=$(curl -s -X GET 'https://api.github.com/repos/actions/runner/releases/latest' | jq -r '.tag_name')
+latest_version=$(echo ${latest_version_label:1})
+runner_file="actions-runner-linux-x64-${latest_version}.tar.gz"
+
+runner_url="https://github.com/actions/runner/releases/download/${latest_version_label}/${runner_file}"
+
+echo "Downloading ${latest_version_label} for ${runner_plat} ..."
+echo $runner_url
+
+curl -O -L ${runner_url}
+
+ls -la *.tar.gz
+
+#---------------------------------------------------
+# extract to runner directory in this directory
+#---------------------------------------------------
+echo
+echo "Extracting ${runner_file} to ./runner"
+
+tar xzf "./${runner_file}" -C runner
